45baa216f0bf599e93bd0278a752969a4a93905a
AlexReshetnik <reshetnik262@gmail.com>
2022-11-09 09:15:01 +0200

    downloading data from a PC

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Sheets_API_Quickstart
- 
+
+During the development of projects, it was often necessary to copy and peek at parts of the code from past projects, so
+I saved everything on the PC, and in git everything was in one repository (AllProjectForStyding), now
+I sorted everything and created a separate repository
---
+++ script.js
@@ -0,0 +1,161 @@
+/* exported gapiLoaded */
+/* exported gisLoaded */
+/* exported handleAuthClick */
+/* exported handleSignoutClick */
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '913154882773-kpr8lh3ujavn1c0jpprb700bff3fdvcp.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyCO2jMhobayviIiSQV0-ZUByyuP1ssq1QY';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+    console.log("gapiLoaded");
+    gapi.load('client', intializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function intializeGapiClient() {
+    console.log("intializeGapiClient");
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+    console.log("gisLoaded");
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '', // defined later
+    });
+    gisInited = true;
+    maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+    console.log("maybeEnableButtons");
+    if (gapiInited && gisInited) {
+        document.getElementById('authorize_button').style.visibility = 'visible';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+    console.log("handleAuthClick");
+    tokenClient.callback = async (resp) => {
+        if (resp.error !== undefined) {
+            throw (resp);
+        }
+        document.getElementById('signout_button').style.visibility = 'visible';
+        document.getElementById('authorize_button').innerText = 'Refresh';
+        await listMajors();
+    };
+
+    if (gapi.client.getToken() === null) {
+        console.log(gapi.client.getToken());
+        // Prompt the user to select a Google Account and ask for consent to share their data
+        // when establishing a new session.
+        tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+        // Skip display of account chooser and consent dialog for an existing session.
+        tokenClient.requestAccessToken({ prompt: '' });
+    }
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+    console.log("handleSignoutClick");
+    const token = gapi.client.getToken();
+    if (token !== null) {
+        google.accounts.oauth2.revoke(token.access_token);
+        gapi.client.setToken('');
+        document.getElementById('content').innerText = '';
+        document.getElementById('authorize_button').innerText = 'Authorize';
+        document.getElementById('signout_button').style.visibility = 'hidden';
+    }
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function create(title, callback) {
+    console.log("create");
+    try {
+        gapi.client.sheets.spreadsheets.create({
+            properties: {
+                title: title,
+            },
+        }).then((response) => {
+            if (callback) callback(response);
+            localStorage.setItem("spreadsheetId", response.result.spreadsheetId)
+            console.log('Spreadsheet ID: ' + response.result.spreadsheetId);
+        });
+    } catch (err) {
+        document.innerText = err.message;
+        return;
+    }
+}
+async function listMajors() {
+    console.log("listMajors");
+    let response;
+    try {
+        response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: localStorage.getItem("spreadsheetId"),
+            range: 'Sheet1!A1:B2',
+        });
+    } catch (err) {
+        console.log(err);
+        // console.log(err.result.error.status);
+        //if (err.result.error.status === "NOT_FOUND") {
+        // console.log(err.result.error.status);
+        await create("MY_DB", (res) => {
+            response = res
+
+            console.log(res);
+        })
+        // }
+
+    }
+    console.log(response);
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+        document.getElementById('content').innerText = 'No values found.';
+        return;
+    }
+    // Flatten to string to display
+    const output = range.values;
+    document.getElementById('content').innerText = output;
+}
